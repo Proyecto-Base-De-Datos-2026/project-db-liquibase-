a2feb3b9e7aba239902279ca6e4d74f42a683329
marlo996 <mamanjarres-2024a@corhuila.edu.co>
2026-05-19 21:46:19 -0500

    Insert veterinario records into database

---
+++ db/changelog/dml/canonical/003-insert-veterinario.sql
@@ -0,0 +1,3 @@
+INSERT INTO veterinario (id_veterinario, nombre, especialidad, correo, activo) VALUES
+(1, 'Dr. Camilo Restrepo', 'general', 'camilo.vet@email.com', TRUE),
+(2, 'Dra. Elena Gómez', 'cirugía', 'elena.vet@email.com', TRUE);
