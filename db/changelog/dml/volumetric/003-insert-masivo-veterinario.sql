b29eab8ead078bb117b5e9c6eb284989d5ef58b0
marlo996 <mamanjarres-2024a@corhuila.edu.co>
2026-05-19 21:50:48 -0500

    Insert multiple veterinario records into database

---
+++ db/changelog/dml/volumetric/003-insert-masivo-veterinario.sql
@@ -0,0 +1,7 @@
+INSERT INTO veterinario (id_veterinario, nombre, especialidad, correo, activo) VALUES
+(10, 'Dr. Alberto Muñoz', 'general', 'alberto.vet@email.com', TRUE),
+(11, 'Dra. María Paula Rojas', 'dermatología', 'maria.vet@email.com', TRUE),
+(12, 'Dr. Fernando Castro', 'cardiología', 'fernando.vet@email.com', TRUE),
+(13, 'Dra. Patricia Delgado', 'ortopedia', 'patricia.vet@email.com', TRUE),
+(14, 'Dr. Jorge Eliécer plazas', 'odontología', 'jorge.vet@email.com', TRUE),
+(15, 'Dra. Clara Inés Murcia', 'otro', 'clara.vet@email.com', TRUE);
